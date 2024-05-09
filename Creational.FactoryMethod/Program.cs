be7c0bab580ba76aaa6ea779b3ca9b1985ce08d2
Mario David Riguera Castillo <mario.riguera@gmail.com>
2024-05-09 14:10:29 +0200

    wip: init commit and adding factory method project.

---
+++ Creational.FactoryMethod/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Creational.FactoryMethod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
